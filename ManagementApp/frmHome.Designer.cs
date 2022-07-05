e716537d1e1f4f96fa31bcf8fd5ad3d271bf2672
blackSkyss <blacksky01102001@gmail.com>
2022-07-05 15:32:22 +0700

    Create form base

--- ManagementApp/frmHome.Designer.cs
+++ ManagementApp/frmHome.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.empToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.employeeManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rewardAndPenaltyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.departmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.posToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.certificateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regulationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -36,20 +45,83 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("UD Digi Kyokasho NP-B", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.DarkCyan;
-            this.label1.Location = new System.Drawing.Point(340, 31);
+            this.label1.Location = new System.Drawing.Point(422, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "HOME PAGE";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empToolStripMenuItem,
+            this.departmentToolStripMenuItem,
+            this.posToolStripMenuItem,
+            this.certificateToolStripMenuItem,
+            this.regulationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(989, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // empToolStripMenuItem
+            // 
+            this.empToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.employeeManagementToolStripMenuItem,
+            this.rewardAndPenaltyToolStripMenuItem});
+            this.empToolStripMenuItem.Name = "empToolStripMenuItem";
+            this.empToolStripMenuItem.Size = new System.Drawing.Size(106, 29);
+            this.empToolStripMenuItem.Text = "Employee";
+            // 
+            // employeeManagementToolStripMenuItem
+            // 
+            this.employeeManagementToolStripMenuItem.Name = "employeeManagementToolStripMenuItem";
+            this.employeeManagementToolStripMenuItem.Size = new System.Drawing.Size(302, 34);
+            this.employeeManagementToolStripMenuItem.Text = "Employee Management";
+            // 
+            // rewardAndPenaltyToolStripMenuItem
+            // 
+            this.rewardAndPenaltyToolStripMenuItem.Name = "rewardAndPenaltyToolStripMenuItem";
+            this.rewardAndPenaltyToolStripMenuItem.Size = new System.Drawing.Size(302, 34);
+            this.rewardAndPenaltyToolStripMenuItem.Text = "Reward And Penalty";
+            // 
+            // departmentToolStripMenuItem
+            // 
+            this.departmentToolStripMenuItem.Name = "departmentToolStripMenuItem";
+            this.departmentToolStripMenuItem.Size = new System.Drawing.Size(123, 29);
+            this.departmentToolStripMenuItem.Text = "Department";
+            // 
+            // posToolStripMenuItem
+            // 
+            this.posToolStripMenuItem.Name = "posToolStripMenuItem";
+            this.posToolStripMenuItem.Size = new System.Drawing.Size(91, 29);
+            this.posToolStripMenuItem.Text = "Position";
+            // 
+            // certificateToolStripMenuItem
+            // 
+            this.certificateToolStripMenuItem.Name = "certificateToolStripMenuItem";
+            this.certificateToolStripMenuItem.Size = new System.Drawing.Size(106, 29);
+            this.certificateToolStripMenuItem.Text = "Certificate";
+            // 
+            // regulationToolStripMenuItem
+            // 
+            this.regulationToolStripMenuItem.Name = "regulationToolStripMenuItem";
+            this.regulationToolStripMenuItem.Size = new System.Drawing.Size(112, 29);
+            this.regulationToolStripMenuItem.Text = "Regulation";
+            // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(989, 540);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "frmHome";
             this.Text = "frmHome";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +130,13 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem empToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem employeeManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rewardAndPenaltyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem departmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem posToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem certificateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regulationToolStripMenuItem;
     }
 }
